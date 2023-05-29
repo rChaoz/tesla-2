9e9e8be6c439d2abf5c84442c879642ad40d81fe
rChaoz <catalin_trand2@yahoo.com>
2023-05-29 17:35:39 +0300

    Arduino: Basic working RC car

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -36,8 +36,27 @@ hd44780_I2Cexp lcd;
 
 // Motors shield
 A4990MotorShield motors;
+#define MOTOR_MAX 300
 
-// ---------- PINs & Definitions ----------
+// ----------- Utility functions ----------
+// ========================================
+
+bool displayedIsConnected = 1;
+void displayMessage(String message) {
+  int len = message.length();
+  lcd.setCursor(0, 1);
+  if (len >= 16) {
+   lcd.print(message.substring(0, 16));
+  } else if (len < 16) {
+    int spaceCount = (16 - len) / 2;
+    for (int i = 0; i < spaceCount; ++i) lcd.write(' ');
+    lcd.print(message);
+    for (int i = 0; i < 16 - len - spaceCount; ++i) lcd.write(' ');
+  }
+}
+
+
+// --------------- Main code --------------
 // ========================================
 
 void setup() {
@@ -59,6 +78,55 @@ void setup() {
   lcd.print("Tesla II");
 }
 
+#if DEBUG
+long lastDisplay = 0;
+#endif
+
 void loop() {
-  
+  bool isConnected = digitalRead(STATE);
+  if (isConnected != displayedIsConnected) {
+    displayedIsConnected = isConnected;
+    if (isConnected) displayMessage("Connected!");
+    else {
+      // Ensure it doesn't keep going if we disconnect while moving
+      motors.setSpeeds(0, 0);
+      displayMessage("No BT connection");
+    }
+  }
+  // Read incoming data
+  if (isConnected && Serial.available()) {
+    String s = Serial.readStringUntil('\r');
+    while (Serial.read() == -1); // make sure to also read the '\n'
+    // S is of format "%lf,%lf", where numbers are -1..+1 and represent the desired speed(x and y axis)
+    int comma = s.indexOf(',');
+    if (comma != -1) {
+      double x = s.substring(0, comma).toDouble();
+      double y = s.substring(comma + 1).toDouble();
+
+      // Display incoming data
+      #if DEBUG
+      long now = millis();
+      if (now > lastDisplay + 1000) {
+        lastDisplay = millis();
+        displayMessage(s);
+      }
+      #endif
+      
+      // Coerce x and y into -1..+1 interval to avoid motor overload
+      if (x < -1) x = -1;
+      if (x > 1) x = 1;
+      if (y < -1) y = -1;
+      if (y > 1) y = 1;
+      // At higher speeds, allow less turning (make it more controllable)
+      x *= 1 - abs(y) * 0.5;
+      if (y >= 0) x = -x;
+      // Calculate power for engines
+      int left = y * MOTOR_MAX - x * MOTOR_MAX;
+      if (left < -MOTOR_MAX) left = -MOTOR_MAX;
+      int right = y * MOTOR_MAX + x * MOTOR_MAX;
+      if (right > MOTOR_MAX) y = MOTOR_MAX;
+      // Send speeds to controller
+      motors.setSpeeds(left, right);
+    }
+  }
 }
